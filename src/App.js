81ea35fea7ae788e9d7e0d3888e5a9be64ce20d8
Murat Sahin <yazilimogren1111@gmail.com>
2021-03-31 16:28:44 +0200

    sidebar component and feed component completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
+import Feed from './Feed';
 import Header from './Header';
 import Sidebar from './Sidebar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
        <Header />
-      <div className="app__body">
+      <div className="app_body">
         <Sidebar />
+        <Feed/>
       </div>
-      {/* app body */}
-      {/* sidebar */}
-      {/* feed */}
+     
       {/* widgets */}
     </div>
   );
